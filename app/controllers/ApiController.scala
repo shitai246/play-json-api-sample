867427b2d0a0d5f89dcf5d343e8e97d19c46cf04
t_shiratori <t_shiratori@fancs.com>
2016-08-03 14:23:26 +0900

    add, json-api sample

---
+++ app/controllers/ApiController.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class ApiController @Inject() extends Controller {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def sample = Action {
+    val json : JsValue = Json.parse("""
+      {
+        "result" : "200",
+        "message" : "success"
+      }
+    """)
+    Ok(json)
+  }
+
+}
